1fd3b7b01e30b69f11d2a17f631001f3be5f8dfe
Brad Fabian <bradleofabian@gmail.com>
2020-10-16 14:58:20 -0400

    styled the header

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal} from 'styled-components';
 import Header from '../components/Header';
-import Meta from '../components/Meta'
+import Meta from '../components/Meta';
+
+const theme = {
+    red: '#FF0000',
+    black: '#393939',
+    grey: '#3a3a3a',
+    lightgrey: '#E1E1E1',
+    offwhite: '#EDEDED',
+    maxWidth: '1000px',
+    bs: '0 12px 24px 0 rgba(0,0,0,0.09)',
+}
+const StyledPage = styled.div`
+background: white;
+color: ${props => props.theme.black};`;
+
+const Inner = styled.div`
+max-width: ${props => props.theme.maxWidth};
+margin: 0 auto;
+padding: 2em;`;
+
+
 export default class Page extends Component {
     
 
     render() {
         return (
-            <div>
+            <ThemeProvider theme={theme}>
+                <StyledPage>
                 <Meta />
                 <Header />
-                {this.props.children}
-            </div>
+                <Inner>{this.props.children}</Inner>
+            </StyledPage>
+            </ThemeProvider>
+            
+                
+           
         )
     }
 }
