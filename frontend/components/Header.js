298adf582fc49dabb85867b8ca9191440e2fb8b6
Brad Fabian <bradleofabian@gmail.com>
2020-10-20 17:23:48 -0400

    set up styling on server

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -4,6 +4,16 @@ import styled from 'styled-components';
 import Router from 'next/router';
 import NProgress from 'nprogress';
 
+Router.onRouteChangeStart= () => {
+NProgress.start();
+}
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+}
+Router.onRouteChangeError = () => {
+  NProgress.done();
+}
+
 const Logo = styled.h1`font-size: 4rem;
 margin-left: 2rem;
 position: relative;
